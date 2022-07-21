a6c31b7505928d51289bd01bf158feefd85b44b5
kostalev <arctic@list.ru>
2022-07-22 02:02:21 +0500

    Урок 3 Задача 3 Блок-схема и Решение

--- exmpl02/Program.cs
+++ exmpl02/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
---
+++ exmpl03/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 3;
+int res = 1;
+
+while (res < num)
+{
+
+    double result = Math.Pow(res, index);
+    Console.Write(result + "  ");
+    res++;
+}
+
+
+
