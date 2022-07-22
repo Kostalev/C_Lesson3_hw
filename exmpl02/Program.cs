ba6aaa706bb724dfce55c503a0db2935048514b9
kostalev <arctic@list.ru>
2022-07-22 15:09:38 +0500

    Добавил Урок 3 Задача 2 Блок-схема и решение

--- exmpl02/Program.cs
+++ exmpl02/Program.cs
@@ -1,1 +1,20 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+
+Double x1, x2, y1, y2, z1, z2, dif;
+
+Console.Write("Введите координаты точки A (x): ");
+x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты точки A (y): ");
+y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты точки A (z): ");
+z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты точки B (x): ");
+x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты точки B (y): ");
+y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты точки B (z): ");
+z2 = Convert.ToDouble(Console.ReadLine());
+
+dif = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine("Расстояние между точками в 3D пространстве {0}", dif);
